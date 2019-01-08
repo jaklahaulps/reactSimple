1aa5f9c30c0a1ea9478fddaeb5cdfd30d57af5ae
jaklahaulps <liu.perry@gmail.com>
2019-01-08 14:13:36 -0500

    Message jsx is added and message list is rendering

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 
 
-
-
-class Messages extends Component {
-    
+class Message extends Component {
+    render(){
+        return (
+            <div className="message">
+                <span className="message-username">{this.props.message.username}</span>
+                <span className="message-content">{this.props.message.content}</span>
+            </div>
+        )
+    }
 
 }
 
-export default Messages;
+export default Message;
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
-import Messages from './Message.jsx';
+import Message from './Message.jsx';
 
 class MessageList extends Component {
 
     render() {
+        
         return (
             <main className="messages">
-                <div className="message">
-                    <span className="message-username">{this.props.messages[0].username}</span>
-                    <span className="message-content">{this.props.messages[0].content}</span>
-                </div>
-                <div className="message">
-                    <span className="message-username">{this.props.messages[1].username}</span>
-                    <span className="message-content">{this.props.messages[1].content}</span>
-                </div>                
+                { this.props.messages.map((message) =>
+            <Message key={message.id} message={message}/>
+        ) }
             </main>
         )
     }
